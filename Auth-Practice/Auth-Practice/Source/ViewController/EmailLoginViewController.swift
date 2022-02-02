6e00b2f4f31e7332df233a24c52e18665a05b3fd
so yeon <alicesysy@swu.ac.kr>
2022-02-02 21:50:36 +0900

    [Feat] 정규식 분기처리 구현

--- Auth-Practice/Auth-Practice/Source/ViewController/EmailLoginViewController.swift
+++ Auth-Practice/Auth-Practice/Source/ViewController/EmailLoginViewController.swift
@@ -13,7 +13,7 @@ import Then
 import Moya
 
 final class EmailLoginViewController: UIViewController {
-
+    
     // MARK: - Properties
     
     private var backButton = UIButton().then {
@@ -29,18 +29,32 @@ final class EmailLoginViewController: UIViewController {
     }
     
     // FIX ME : 디자인 시스템 적용
-    private var emailTextField = UITextField().then {
-        $0.placeholder = "이메일"
+    private var emailTextField = BDSTextField().then {
+        $0.setPlaceholder(placeholder: "이메일")
     }
     
-    private var passwordTextField = UITextField().then {
-        $0.placeholder = "비밀번호"
+    private var passwordTextField = BDSTextField().then {
+        $0.setPlaceholder(placeholder: "비밀번호")
     }
     
-    private var loginButton = UIButton().then {
-        $0.setTitle("로그인", for: .normal)
-        $0.setTitleColor(.white, for: .normal)
-        $0.backgroundColor = .black
+    private var emailWarningLabel = UILabel().then {
+        $0.textColor = .red100
+        $0.font = .Pretendard(type: .regular, size: 14)
+        $0.isHidden = true
+    }
+    
+    private var passwordWarningLabel = UILabel().then {
+        $0.textColor = .red100
+        $0.font = .Pretendard(type: .regular, size: 14)
+        $0.isHidden = true
+    }
+    
+    private var loginButton = BDSButton().then {
+        $0.setBtnColors(normalBgColor: .gray300, normalFontColor: .white, activatedBgColor: .black200, activatedFontColor: .white)
+        $0.setTitleWithStyle(title: "로그인", size: 17.0)
+        $0.isActivated = true
+        $0.isEnabled = true
+        $0.addTarget(self, action: #selector(touchUpLoginButton), for: .touchUpInside)
     }
     
     private var forgetPasswordButton = UIButton().then {
@@ -60,16 +74,30 @@ final class EmailLoginViewController: UIViewController {
         super.viewDidLoad()
         configUI()
         setupLayout()
+        bind()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
     }
     
     // MARK: - InitUI
     
     private func configUI() {
         view.backgroundColor = .white
+        
+        emailTextField.becomeFirstResponder()
     }
     
     private func setupLayout() {
-        [backButton, emailLoginLabel, emailTextField, passwordTextField, loginButton, forgetPasswordButton].forEach {
+        [backButton,
+         emailLoginLabel,
+         emailTextField,
+         emailWarningLabel,
+         passwordTextField,
+         passwordWarningLabel,
+         loginButton,
+         forgetPasswordButton].forEach {
             view.addSubview($0)
         }
         
@@ -87,11 +115,23 @@ final class EmailLoginViewController: UIViewController {
         emailTextField.snp.makeConstraints {
             $0.top.equalTo(emailLoginLabel.snp.bottom).offset(42)
             $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(42)
+        }
+        
+        emailWarningLabel.snp.makeConstraints {
+            $0.top.equalTo(emailTextField.snp.bottom).offset(4)
+            $0.leading.equalToSuperview().inset(28)
         }
         
         passwordTextField.snp.makeConstraints {
             $0.top.equalTo(emailTextField.snp.bottom).offset(40)
             $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(42)
+        }
+        
+        passwordWarningLabel.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(4)
+            $0.leading.equalToSuperview().inset(28)
         }
         
         loginButton.snp.makeConstraints {
@@ -108,10 +148,92 @@ final class EmailLoginViewController: UIViewController {
     
     // MARK: - Custom Method
     
+    private func bind() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    private func isValidEmail(testStr: String) -> Bool {
+        let emailRegEx = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,20}$"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
     // MARK: - @objc
     
     @objc func touchUpBackButton() {
         navigationController?.popViewController(animated: true)
     }
+    
+    @objc func touchUpLoginButton() {
+        guard let text = emailTextField.text else { return }
+        
+        if !emailTextField.hasText && !passwordTextField.hasText {
+            emailWarningLabel.isHidden = false
+            emailWarningLabel.text = "이메일을 입력해주세요"
+            
+            passwordWarningLabel.isHidden = false
+            passwordWarningLabel.text = "비밀번호를 입력해주세요"
+        } else {
+            if !isValidEmail(testStr: text) {
+                emailWarningLabel.isHidden = false
+                emailWarningLabel.text = "올바르지 않은 이메일 형식입니다"
+                passwordWarningLabel.isHidden = true
+            } else {
+                if passwordTextField.hasText {
+                    // 서버 통신 후 분기 처리
+                    
+                    // TODO REMOVE
+                    emailWarningLabel.isHidden = true
+                    passwordWarningLabel.isHidden = true
+                } else {
+                    emailWarningLabel.isHidden = true
+                    passwordWarningLabel.isHidden = false
+                    passwordWarningLabel.text = "비밀번호를 입력해주세요"
+                }
+            }
+        }
+        
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
 }
 
+// MARK: - TextField Delegate
+
+extension EmailLoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let text = emailTextField.text else { return true }
+        
+        if !emailTextField.hasText && !passwordTextField.hasText {
+            emailWarningLabel.isHidden = false
+            emailWarningLabel.text = "이메일을 입력해주세요"
+            
+            passwordWarningLabel.isHidden = false
+            passwordWarningLabel.text = "비밀번호를 입력해주세요"
+        } else {
+            if !isValidEmail(testStr: text) {
+                emailWarningLabel.isHidden = false
+                emailWarningLabel.text = "올바르지 않은 이메일 형식입니다"
+                passwordWarningLabel.isHidden = true
+            } else {
+                if passwordTextField.hasText {
+                    // 서버 통신 후 분기 처리
+                    
+                    // TODO REMOVE
+                    emailWarningLabel.isHidden = true
+                    passwordWarningLabel.isHidden = true
+                } else {
+                    emailWarningLabel.isHidden = true
+                    passwordWarningLabel.isHidden = false
+                    passwordWarningLabel.text = "비밀번호를 입력해주세요"
+                }
+            }
+        }
+        
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        return true
+    }
+}
