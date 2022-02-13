2bd363b19d6fcbe1cc3ac184202b9139356b37e5
so yeon <alicesysy@swu.ac.kr>
2022-02-14 03:40:55 +0900

    [Del] 사용하지 않는 네트워크 상수값 삭제

--- Auth-Practice/Auth-Practice/Network/Base/NetworkConstant.swift
+++ Auth-Practice/Auth-Practice/Network/Base/NetworkConstant.swift
@@ -11,11 +11,10 @@ struct NetworkConstant {
     
     static let noTokenHeader = ["Content-Type": "application/json"]
     static let hasTokenHeader = ["Content-Type": "application/json",
-                                 "token": NetworkConstant.accessToken]
+                                 "accesstoken": Const.UserDefaultsKey.accessToken]
+    static let hasRefreshTokenHedaer = ["Content-Type": "application/json",
+                                        "accesstoken": Const.UserDefaultsKey.refreshToken,
+                                        "refreshtoken": Const.UserDefaultsKey.refreshToken]
     
     static var accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJzdWJpbjA3MjNAYmVmb3JlZ2V0LmNvbSIsIm5pY2siOiLtj6zrppAiLCJpZEZpcmViYXNlIjoiaXNRM1kzVU4xSVlqdmQzMXpsZk5Bd2FHejFtMSIsImlhdCI6MTY0MjQzNTEzMSwiZXhwIjoxNjQzNjQ0NzMxLCJpc3MiOiJjaGFud29vIn0.zIK0c8Gq1f_GcJ_UjkwABWfXQ5UbVSU5M69uEqZhKkc"
-    
-    //    static var accessToken = "\(UserDefaults.standard.value(forKey: "token") as? String ?? "")"
-    
-    static var name = "\(UserDefaults.standard.value(forKey: "name") as? String ?? "")"
 }
