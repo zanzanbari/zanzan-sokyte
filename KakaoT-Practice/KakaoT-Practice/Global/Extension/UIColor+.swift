e81cc78dc584fab5da5567223082d1b55298fa10
so yeon <alicesysy@swu.ac.kr>
2022-02-27 04:09:44 +0900

    [Add] UIColor+ 추가 (#3)

---
+++ KakaoT-Practice/KakaoT-Practice/Global/Extension/UIColor+.swift
@@ -0,0 +1,81 @@
+//
+//  UIColor+.swift
+//  KakaoT-Practice
+//
+//  Created by soyeon on 2022/02/27.
+//
+
+import UIKit
+
+extension UIColor {
+    // black
+    @nonobjc class var black100: UIColor {
+        return UIColor(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var black200: UIColor {
+        return UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
+    }
+    
+    // navy
+    @nonobjc class var navy100: UIColor {
+        return UIColor(red: 39.0 / 255.0, green: 44.0 / 255.0, blue: 56.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var navy200: UIColor {
+        return UIColor(red: 136.0 / 255.0, green: 140.0 / 255.0, blue: 148.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var navy300: UIColor {
+        return UIColor(red: 164.0 / 255.0, green: 167.0 / 255.0, blue: 172.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var navy400: UIColor {
+        return UIColor(red: 245.0 / 255.0, green: 246.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
+    }
+    
+    // gray
+    @nonobjc class var gray100: UIColor {
+        return UIColor(red: 149.0 / 255.0, green: 149.0 / 255.0, blue: 150.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray200: UIColor {
+        return UIColor(red: 196.0 / 255.0, green: 197.0 / 255.0, blue: 198.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray300: UIColor {
+        return UIColor(red: 216.0 / 255.0, green: 218.0 / 255.0, blue: 220.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray400: UIColor {
+        return UIColor(red: 238.0 / 255.0, green: 238.0 / 255.0, blue: 239.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray500: UIColor {
+        return UIColor(red: 249.0 / 255.0, green: 249.0 / 255.0, blue: 240.0 / 255.0, alpha: 1.0)
+    }
+    
+    // white
+    @nonobjc class var white: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    }
+    
+    // blue
+    @nonobjc class var blue100: UIColor {
+        return UIColor(red: 74.0 / 255.0, green: 127.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var blue200: UIColor {
+        return UIColor(red: 55.0 / 255.0, green: 126.0 / 255.0, blue: 198.0 / 255.0, alpha: 1.0)
+    }
+    
+    // red
+    
+    @nonobjc class var red100: UIColor {
+        return UIColor(red: 204.0 / 255.0, green: 72.0 / 255.0, blue: 66.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var red200: UIColor {
+        return UIColor(red: 146.0 / 255.0, green: 56.0 / 255.0, blue: 55.0 / 255.0, alpha: 1.0)
+    }
+}
