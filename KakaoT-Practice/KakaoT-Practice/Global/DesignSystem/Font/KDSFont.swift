48f2107b63785959e81bd474b491c4b212c53668
so yeon <alicesysy@swu.ac.kr>
2022-03-03 02:08:06 +0900

    [Feat] 카카오T 디자인 시스템 구현 (#3)

---
+++ KakaoT-Practice/KakaoT-Practice/Global/DesignSystem/Font/KDSFont.swift
@@ -0,0 +1,20 @@
+//
+//  KDSFont.swift
+//  KakaoT-Practice
+//
+//  Created by soyeon on 2022/03/02.
+//
+
+import UIKit
+
+public enum KDSFont {
+    static let body1 = UIFont.systemFont(ofSize: 17, weight: .regular)
+    static let body2 = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let body3 = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let body4 = UIFont.systemFont(ofSize: 15, weight: .regular)
+    static let body5 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let body6 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let body7 = UIFont.systemFont(ofSize: 13, weight: .regular)
+    static let body8 = UIFont.systemFont(ofSize: 12, weight: .regular)
+    static let body9 = UIFont.systemFont(ofSize: 10, weight: .regular)
+}
