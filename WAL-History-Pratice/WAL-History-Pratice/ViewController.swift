950bc49c7f64ccd37561b9c80f28ec1a06d96ff6
so yeon <alicesysy@swu.ac.kr>
2022-04-27 02:01:00 +0900

    [Add] 프로젝트 및 SPM 추가 (#12)

---
+++ WAL-History-Pratice/WAL-History-Pratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WAL-History-Pratice
+//
+//  Created by 소연 on 2022/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
