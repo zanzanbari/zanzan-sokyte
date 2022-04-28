ee20efc4bb5da557884dcf00c62bb577464aa433
so yeon <alicesysy@swu.ac.kr>
2022-04-28 23:55:11 +0900

    [Chore] 더미데이터 Response Optional 처리 (#12)

--- WAL-History-Pratice/WAL-History-Pratice/HistoryResponse.swift
+++ WAL-History-Pratice/WAL-History-Pratice/HistoryResponse.swift
@@ -18,4 +18,20 @@ struct HistoryResponse: Codable {
 struct HistoryData: Codable {
     let sendingDate, content, recieveDate: String
     let hidden: Bool?
+    
+    enum CodingKeys: String, CodingKey {
+        case sendingDate
+        case content
+        case recieveDate
+        case hidden
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        sendingDate = (try? values.decode(String.self, forKey: .sendingDate)) ?? ""
+        content = (try? values.decode(String.self, forKey: .content)) ?? ""
+        recieveDate = (try? values.decode(String.self, forKey: .recieveDate)) ?? ""
+        hidden = (try? values.decode(Bool.self, forKey: .hidden)) ?? false
+    }
 }
